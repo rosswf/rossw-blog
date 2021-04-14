0fb4920add6c44c6b34efc8dc95018fd4152dcae
Ross <ross@rossw.co.uk>
2021-04-14 13:54:26 +0100

    Disable relative urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,9 +34,6 @@ DEFAULT_PAGINATION = 5
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
-
 FEED_DOMAIN = SITEURL
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 
